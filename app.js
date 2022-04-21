441c6bd1ed5368082115a4763e7c1d7780e0dd24
Maria <achigarpia@gmail>
2022-04-21 19:20:08 -0300

    commit

--- app.js
+++ app.js
@@ -1,53 +1,50 @@
-//Priero debo capturar el evento submit, Con el escucha "addEventListener"
+//agrego un escucha de tipo submit al form
 document.getElementById("formTareas").addEventListener("submit", guardarTarea);
+
 function guardarTarea(e) {
   e.preventDefault(); //cancela el comportamiento por defecto de refrescar la pág
-  let title = document.getElementById("title").value;
-  let descrip = document.getElementById("description").value;
+  let title = document.getElementById("title").value; //linea 28
+  let descrip = document.getElementById("description").value; //linea 31
   const tarea = {
     title, // es lo mismo que poner title:title
     descrip,
   };
-  if (localStorage.getItem("tareas") === null) {
-    // si en la variable tarea del localStorage no hay nada...creo
+  if (localStorage.getItem("tareas")) {
+    // si esta en el localStorage, entonces los obtengo, actualizo y vuelvo a guardarTarea
+    let tasks = JSON.parse(localStorage.getItem("tareas"));
+    tasks.push(tarea);
+    localStorage.setItem("tareas", JSON.stringify(tasks));
+  } else {
+    // si en el localStorage no esta guardado aún...creo en array y lo guardo
     let tasks = [];
     tasks.push(tarea);
-    localStorage.setItem("tareas", JSON.stringify(tasks)); //JSON es un método del navegador que permite convertir Obj a String
-  } else {
-    // si ya hay tareas, entonces los obtengo, actualizo y vuelvo a guardarTarea
-    let tasks = JSON.parse(localStorage.getItem("tareas")); // obtengo las tareas que estan guardadas en el localStorage, las paso a Objeto otra vez
-    tasks.push(tarea);
-    localStorage.setItem("tareas", JSON.stringify(tasks)); //paso mi array a string y lo vuelvo a guardar en el localStorage
+    localStorage.setItem("tareas", JSON.stringify(tasks));
   }
-  obtenerTareas();
+  mostrarTareas();
   document.getElementById("formTareas").reset();
 }
 
-function obtenerTareas() {
+function mostrarTareas() {
   let tareas = JSON.parse(localStorage.getItem("tareas"));
-  let tareasEnHtml = document.getElementById("tareas");
+  let tareasEnHtml = document.getElementById("tareas"); // linea 40
   tareasEnHtml.innerHTML = ""; //lo limpiamos en caso de que haya datos
-  console.log(tareas);
   tareas.forEach((e) => {
-    console.log(e);
     tareasEnHtml.innerHTML += `<div class = "card mb-4">
-    <div class = "card-body">
-    <p>${e.title} - ${e.descrip}</p>
-    <a class = "btn btn-danger" onclick="eliminarTareas('${e.title}')">Eliminar</a>
-    </div>
-    </div>`;
+                                <div class = "card-body">
+                                  <p>${e.title} - ${e.descrip}</p>
+                                  <a class = "btn btn-danger" onclick="eliminarTareas('${e.title}')">Eliminar</a>
+                                </div>
+                              </div>`;
   });
 }
 
 function eliminarTareas(title) {
-  let tareas = JSON.parse(localStorage.getItem("tareas")); //obtuve
+  let tareas = JSON.parse(localStorage.getItem("tareas"));
   tareas.map((e) => {
     if (e.title == title) {
-      console.log(e.descrip);
       tareas.splice(e.index, 1); //elimino el que me mandan por param
     }
-    console.log(e);
   });
-  localStorage.setItem("tareas", JSON.stringify(tareas)); //vuelvo  guardar el el local
-  obtenerTareas();
+  localStorage.setItem("tareas", JSON.stringify(tareas));
+  mostrarTareas();
 }
